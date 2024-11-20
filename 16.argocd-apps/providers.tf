a759b378996e46b8b7ee6a34910d67543ad21417
Alexey B <alexey.b@tlf.com>
2024-11-20 17:18:56 +0200

    initial comit. Wed Nov 20 17:18:56 EET 2024

---
+++ 16.argocd-apps/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.25.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+  }
+}
+
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = var.kube_context
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = "~/.kube/config"
+    config_context = var.kube_context
+  }
+}
